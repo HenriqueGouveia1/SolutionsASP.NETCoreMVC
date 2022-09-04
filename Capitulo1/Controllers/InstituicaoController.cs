0215bde088e4a853de7cb8a629251dd77652c79f
henriquegouveia1 <henriquegs002@gmail.com>
2022-09-04 10:31:29 -0300

    Adicionar arquivos de projeto.

---
+++ Capitulo1/Controllers/InstituicaoController.cs
@@ -0,0 +1,51 @@
+﻿using Capitulo1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capitulo1.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<InstituicaoModel> instituicoes = new List<InstituicaoModel>()
+        {
+            new InstituicaoModel()
+            {
+                InstituicaoId = 0,
+                Nome = "Unipe",
+                Endereco = "Rua ali"
+            }, 
+            new InstituicaoModel()
+            {
+                InstituicaoId = 1,
+                Nome = "FPB",
+                Endereco = "Rua ala"
+            } ,
+            new InstituicaoModel()
+            {
+                InstituicaoId = 2,
+                Nome = "UNIESP",
+                Endereco = "Rua alibaba"
+            }
+
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+        //GET Create 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(InstituicaoModel instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoId = instituicoes.Select(i => i.InstituicaoId).Max() + 1;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Capitulo1/Models/InstituicaoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Capitulo1.Models
+{
+    public class InstituicaoModel
+    {
+        public long? InstituicaoId { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }    
+    }
+}
